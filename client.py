1b731e22bdadf93f97c75f064c179dd998dc73bb
eMostyn <56824767+eMostyn@users.noreply.github.com>
2020-11-19 18:58:19 +0000

    sorted multi-client messaging
    Clients can now send messages that are then sent to other clients.
    Join working fully
    Leave message needs tweaking
    Error detection and log file needs fixing.

--- client.py
+++ client.py
@@ -1,14 +1,35 @@
-import socket as sock, sys
+import socket as sock, sys,select, msvcrt
 
 uName,host,port = sys.argv[1:]
 port = int(port)
 print(uName,host,port)
+def getMsg():
+    print("You:")
+    
 def start_client():
     print("started")
     cs = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
     cs.connect((host,port))
     cs.sendto(uName.encode(), (host,port))
+    while(True):
+        read_sockets, write_sockets, error_sockets = select.select([cs] , [], [],0)	
+        for s in read_sockets:
+            data = s.recv(4096)
+            print(data.decode())
+        num = 0
+        done = False
+        while not done and num<500:
+            num += 1
+
+            if msvcrt.kbhit():
+                msg = input("You:")
+                done = True
+                msg = msg.encode()
+                cs.send(msg)
 
 
 if __name__ == "__main__":
     start_client()
+
+
+
--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket as sock, sys, select
 port = int(sys.argv[1])
 conns = {}
 host = "127.0.0.1"
+welcomeMessage = "Welcome to the chat server!"
 def start_server():
     with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as s:
         #s.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
@@ -10,20 +11,47 @@ def start_server():
         print("Server Ready")
         s.listen(5)
         print('listening on', (host, port))
-        conns["SERVER"] =  s
-        while True:
-            rSocks,wSocks,eSocks = select.select(conns.values(),[],[])
+        conns[s] =  "SERVER"
+        while 1:
+            rSocks,wSocks,eSocks = select.select(conns.keys(),[],[])
             for rSock in rSocks:
                 if rSock == s:
                     newSock,addr = s.accept()
                     uName = newSock.recv(1024).decode()
-                    conns[uName] = newSock
-                    print(addr)
-                    print(uName + " has connected to the server. With IP: "+addr[0] + " At Port: "+str(addr[1])) 
+                    conns[newSock] = uName
+                    newSock.send(welcomeMessage.encode())
+                    print(uName + " has connected to the server. With IP: "+addr[0] + " At Port: "+str(addr[1]))
+                    joinMessage(uName,newSock,s)
+                else:
+                    data = rSock.recv(1024).decode()
+                    if data:
+                        print(data)
+                        sendMessage(conns[rSock],data,rSock,s)
+                    else:
+                        uName = conns[rSock]
+                        print(uName + " has left")
+                        leaveMessage(uName,s)
+                        rSock.close()
+                        del conns[rSock]
 
+def joinMessage(uName,sock,server):
+    message = "["+uName+"]"+" has joined!"
+    for client in conns.keys():
+        if((client != server)&(client!=sock)):
+            client.send(message.encode())
 
-def newClient(sock,addr):
-    print(sock,addr)
+def leaveMessage(uName,server):
+    message = "["+uName+"]"+" has left!"
+    for client in conns.keys():
+        if((client != server)):
+            client.send(message.encode())
+            
+def sendMessage(uName,message,sock,server):
+    message = uName +": " + message
+    for client in conns.keys():
+        if((client != server)&(client!=sock)):
+            client.send(message.encode())
+        
 
 
 
