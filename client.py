15cb0b16cfa5e4d80959505bfb3a99d3152636f1
eMostyn <56824767+eMostyn@users.noreply.github.com>
2021-02-14 14:05:15 +0000

    Final submission

--- client.py
+++ client.py
@@ -1,33 +1,245 @@
-import socket as sock, sys,select, msvcrt
+import socket as sock
+import sys
+import select
+import tkinter as tk
 
 uName,host,port = sys.argv[1:]
 port = int(port)
-print(uName,host,port)
-def getMsg():
-    print("You:")
+interface = tk.Tk()
+interface.title("Chat Window")
+
+def send_message(userInput,chat,client):
+    #Get the contents of the input box
+    msg = userInput.get("1.0","end")
+    encodedMsg = msg.encode()
+    #Split the msg up to access command
+    split = msg.split(" ")
+    # If there was no space the command is the entire thing
+    if(len(split)==1):
+        command = split[0][:-1]
+    # Else its the first of the split
+    else:
+        command = split[0]
+    # If the msg isnt blank, stops client sending unnecesary messages
+    if(not msg.isspace()):
+        if(command == "/say"):
+            # Send the message and put contents into chat box
+            client.send(encodedMsg)
+            chat.insert("end","<You> : "+msg[len(command)+1:])
+
+        elif(command == "/msg"):
+            if(len(split)>2):
+                # Split the message and recipient and send to server
+                client.send(encodedMsg)
+                recipient = split[1]
+                chat.insert("end","<You -> "+recipient+">: "+msg[len(command)+len(recipient)+2:],"whisper")
+            # Either recipient or message wasnt specified
+            else:
+              chat.insert("end","Please enter a username & message \n","error")
+        else:
+            # Nothing special to be done so send message
+            client.send(encodedMsg)
+        # Clear the input
+        userInput.delete("1.0","end")
+            
+    
+
+def update_chat(client,chat,interface,userList):
+    try:
+        # Get sockets
+        rSocks,wSocks,eSocks = select.select([client],[],[],1)
+        for s in rSocks:
+            # Incoming message from server
+            data = s.recv(4096)
+            if not data :
+                print('\nDisconnected from chat server')
+                sys.exit()
+            else :
+                #Decode the data and split it to get command and message part
+                data= data.decode()
+                split  = data.split(" ")
+                command = split[0]
+                message = data[len(command)+1:]
+                #Another users sent message, print it
+                if command == "/say":
+                    chat.insert("end",message)
+                #Server returned list of other users
+                elif command == "/list":
+                    #Split the usernames
+                    otherUsers = data[len(command)+1:].split(" ")
+                    #Update the relevant ui box
+                    updateUserList(userList, otherUsers)
+                # User messaged client directly
+                elif command == "/msg":
+                    # Insert it to ui box, with "whisper" tag to make different colour
+                    chat.insert("end",message,"whisper")
+                # User has joined/left/changed username
+                elif command == "/info":
+                    #If multiple messages sent quickly they arrive as one data with multiple lines
+                    #Split into lines
+                    lines = data.split("\n")
+                    for i in range(0,len(lines)):
+                      # Split the line into words
+                      split = lines[i].split(" ")
+                      # Get command and message
+                      command = split[0]
+                      message = lines[i][len(command)+1:]
+                      # Insert only the message part
+                      chat.insert("end",message,"info")
+                      # Add new lines to all except the last line
+                      if(i!= len(lines)-1):
+                          chat.insert("end","\n")
+                # Server returns an error regarding a previous message
+                elif command == "/error":
+                    # Again, if multiple sent can be multiple lines
+                    lines = data.split("\n")
+                    # Split the lines and show seperately, as above
+                    for i in range(0,len(lines)):
+                      split = lines[i].split(" ")
+                      command = split[0]
+                      message = lines[i][len(command)+1:]
+                      chat.insert("end",message,"error")
+                      if(i!= len(lines)-1):
+                          chat.insert("end","\n")
+        # Recall this update every second
+        interface.after(1000, lambda: update_chat(client,chat,interface,userList))
+    # Error has occured in the client, print the error message and close
+    except Exception as e:
+        print("Quitting due to: " + str(e))
+        client.close()
+        sys.exit()
     
 def start_client():
-    print("started")
+    # Open the socket
     cs = sock.socket(sock.AF_INET, sock.SOCK_STREAM)
-    cs.connect((host,port))
-    cs.sendto(uName.encode(), (host,port))
-    while(True):
-        read_sockets, write_sockets, error_sockets = select.select([cs] , [], [],0)	
-        for s in read_sockets:
-            data = s.recv(4096)
-            print(data.decode())
-        num = 0
-        done = False
-        while not done and num<500:
-            num += 1
+    # Try connect, print error message if cannot connect
+    try:
+        cs.connect((host,port))
+        cs.sendto(uName.encode(), (host,port))
+    except Exception as e:
+        print("An error has occured: " + str(e)+ ".\nEither the server isn't running or the port/host addresses are incorrect.")
+        sys.exit()
+    # Create the ui
+    interface,chatWindow,userList = createInterface(cs)
+    # Update the ui 
+    interface.after(1000, lambda: update_chat(cs,chatWindow,interface,userList))
+    # Show user help commands
+    printCommands()
+    # Shows interface on screen
+    interface.mainloop()
+    update_chat(cs,chatWindow,interface,userList)
+    # UI has been closed, quit
+    print("Quitting")
+    sys.exit()
 
-            if msvcrt.kbhit():
-                msg = input("You:")
-                done = True
-                msg = msg.encode()
-                cs.send(msg)
+def createInterface(cs):
+    #Creates the scrollbar for the chat window
+    scrollbarY = tk.Scrollbar(interface)
+    scrollbarY.grid(row=0,column=4,sticky='ns')
+
+    #Creates the chat window, sets it to top of screen
+    chatWindow = tk.Text(interface, bd=1,width="50", height="8", font=("Gill Sans MT", 23),background = "#363194", foreground="#00ffe5",yscrollcommand=scrollbarY.set,wrap="word")
+    chatWindow.grid(row=0, column=0,columnspan = 3)
+    #Adds some different text colours for different types of messages
+    chatWindow.tag_config('info', foreground="yellow")
+    chatWindow.tag_config('whisper', foreground= "white")
+    chatWindow.tag_config('error', foreground= "red")
+
+    #Sets the scrollbar to work on the chat window
+    scrollbarY.config(command=chatWindow.yview)
+    
+    #Top menu bar
+    menubar = tk.Menu(interface)
+    #Adds the option dropdown
+    optionMenu = tk.Menu(menubar, tearoff=0)
+    #Adds the options of the dropdown
+    optionMenu.add_command(label="Help", command=printCommands)
+    optionMenu.add_command(label="Disconnect",command = leaveChat)
+    menubar.add_cascade(label="Options", menu=optionMenu)
+    #Sets the menu to the menubar
+    interface.config(menu=menubar)
+
+    #Create user input box
+    userInput = tk.Text(interface,bd=1,width="50", height="8",font=("Gill Sans MT", 16))
+    userInput.grid(row=1, column=1,sticky = "sw",rowspan = 3)
+
+    #Creates submit button
+    submitButton = tk.Button(interface, text ="Submit", command= lambda: send_message(userInput,chatWindow,cs),width = "20",height= "2",font=("Gill Sans MT",12))
+    submitButton.grid(row = 1,column=2,sticky = "ew")
+
+    #Creates label for userlist
+    userListLabel = tk.Label(interface,text="Current Users",height ="2",font=("Gill Sans MT",12))
+    userListLabel.grid(row = 1,column = 0,sticky = "n")
+
+    #Adds button to update the user list
+    updateUsersButton = tk.Button(interface, text="Update Users",command = lambda:getUsers(cs),height = "1",font=("Gill Sans MT",12))
+    updateUsersButton.grid(row = 1, column =0,sticky = "s")
+    
+    #User list which updates to show connected users
+    userList = tk.Label(interface,text = "",font=("Gill Sans MT",12))
+    userList.grid(row=2,column = 0,sticky = "n",rowspan = 2)
+
+    #Create the change username label 
+    changeUsernameLabel = tk.Label(interface,text="Change Username",height ="2",font=("Gill Sans MT",12))
+    changeUsernameLabel.grid(row = 2,column = 2,sticky = "n")
+    changeUsernameInput = tk.Text(interface,bd=1,height="1",width = "25",font=("Gill Sans MT",12))
+    changeUsernameInput.grid(row=2,column = 2,sticky = "s")
+
+    #Button to update username to contents of input
+    userNameupdateButton = tk.Button(interface,text= "Update Name",width = "20",height= "2",font=("Gill Sans MT",12),command = lambda:updateUsername(cs,changeUsernameInput))
+    userNameupdateButton.grid(row=3,column = 2)
+    
+    return interface,chatWindow,userList
 
 
+
+def getUsers(cs):
+    #Send relevant message to get users
+    message = "/list"
+    encodedMsg = message.encode()
+    cs.send(encodedMsg)
+    
+    
+
+def updateUserList(userList, otherUsers):
+    #Gets the list of users and updates the list
+    toSet = ""
+    for user in otherUsers:
+        toSet += user + "\n"
+    userList['text']  = toSet
+
+
+def updateUsername(cs,nameInput):
+    #Set the relevant message and send to server
+    message = "/info " + nameInput.get("1.0","end")
+    cs.send(message.encode())
+    #Clear input
+    nameInput.delete("1.0","end")
+
+def printCommands():
+    #Create the help window
+    helpWindow = tk.Tk()
+    helpWindow.title("Help")
+    #Always on top of chat window
+    helpWindow.attributes("-topmost", True)
+    #Lines represent help about commands
+    lines = []
+    lines.append("/say [msg] - Broadcasts message to all users \n")
+    lines.append("/msg [recipient] [msg] - Whisper the message to one user \n")
+    lines.append("/info [new name] - Change your username to a new name \n")
+    lines.append("/list - List all currently connected users (excluding yourself)")
+    #Create the text box to print text to
+    helpText = tk.Text(helpWindow,bd=1,height = "4",width = "50",background = "#363194",foreground = "yellow",font=("Gill Sans MT",16))
+    #For each of the line print it
+    for line in lines:
+        helpText.insert("end",line)
+    helpText.grid(row=0,column = 0)
+    helpWindow.mainloop()
+
+def leaveChat():
+    #Disconnected so close program
+    sys.exit()
+    
 if __name__ == "__main__":
     start_client()
 
--- server.py
+++ server.py
@@ -1,59 +1,189 @@
-import socket as sock, sys, select
+import socket as sock
+import sys
+import select
+import time
+
 port = int(sys.argv[1])
 conns = {}
 host = "127.0.0.1"
-welcomeMessage = "Welcome to the chat server!"
+welcomeMessage = "/info Welcome to the chat server!" + "\n"
+
+
 def start_server():
+    #Creates the log file if not existing, opens if for reading if it does
+    logFile = open("server.log.", "w")
     with sock.socket(sock.AF_INET, sock.SOCK_STREAM) as s:
-        #s.setsockopt(sock.SOL_SOCKET, sock.SO_REUSEADDR, 1)
+        #Start the server, bind it to the specified port and address and listen out for any connections
         print("Started")
-        s.bind((host,port))
+        s.bind((host, port))
         print("Server Ready")
-        s.listen(5)
+        logFile.write("[" + getTime() + "] " + "Server Opened \n")
+        s.listen()
         print('listening on', (host, port))
-        conns[s] =  "SERVER"
+        conns[s] = "SERVER"
         while 1:
-            rSocks,wSocks,eSocks = select.select(conns.keys(),[],[])
+            #Get sockets
+            try:
+                rSocks, wSocks, eSocks = select.select(conns.keys(), [], [], 1)
+            # Server Closed
+            except KeyboardInterrupt:
+                print("Server Closed")
+                logFile.write("[" + getTime() + "] " + "Server Closed \n")
+                logFile.close()
+                sys.exit()
+
+            # In the case the server closes unexpectedly/crashes
+            except Exception as e:
+                #Print error to console & log file
+                print("[" + getTime() + "] " + str(e) + "\n")
+                logFile.write("[" + getTime() + "] " + str(e) + "\n")
+                logFile.close()
+                #Attempt to close all remaining sockets
+                for socket in conns.keys():
+                    socket.close()
+                sys.exit()
+            #For each listening socket
             for rSock in rSocks:
+                #New connection 
                 if rSock == s:
-                    newSock,addr = s.accept()
+                    #Accept the connection, add the username to the dictionary of connections
+                    newSock, addr = s.accept()
                     uName = newSock.recv(1024).decode()
                     conns[newSock] = uName
-                    newSock.send(welcomeMessage.encode())
-                    print(uName + " has connected to the server. With IP: "+addr[0] + " At Port: "+str(addr[1]))
-                    joinMessage(uName,newSock,s)
-                else:
-                    data = rSock.recv(1024).decode()
-                    if data:
-                        print(data)
-                        sendMessage(conns[rSock],data,rSock,s)
+                    #Makes sure username cannot initally be blank "" or " " etc
+                    if(not uName.isspace()):
+                        #Send the welcome message and then print the new user message to console & logfile
+                        newSock.send(welcomeMessage.encode())
+                        print(uName + " has connected to the server. With IP: " + addr[0] + " At Port: " + str(addr[1]))
+                        logFile.write("[" + getTime() + "] " + uName + " has connected to the server. With IP: " + addr[0] + " At Port: " + str(addr[1]) + "\n")
+                        #Inform other users of new user
+                        joinMessage(uName, newSock, s)
+                    #Send an error message 
                     else:
+                        errorMessage = "/error Please set a valid username"
+                        newSock.send(errorMessage.encode())
+                #Existing user sends message
+                else:
+                    try:
+                        data = rSock.recv(1024)
+                        #Data is valid
+                        if data:
+                            #Get the data, from that the username
+                            data = data.decode()
+                            uName = conns[rSock]
+                            #Split the data to get command & message
+                            split = (data.strip()).split(" ")
+                            command = split[0]
+                            message = data[len(command)+1:]
+                            #Send the list of users
+                            if(command == "/list"):
+                                sendUsers(uName, rSock)
+                            #User is updating username
+                            elif(command == "/info"):
+                                    #Gets the new username
+                                    newUser = message.split("\n")[0]
+                                    # If the username is not currently in use
+                                    if(newUser not in conns.values()):
+                                        #Inform the users of the change and update dict
+                                        updateUsers(uName, newUser, rSock, s)
+                                        logFile.write("[" + getTime() + "] " + uName + " is now " + newUser + "\n")
+                                        conns[rSock] = newUser                                       
+                                        #Tell user 
+                                        rSock.send("/info Username changed successfully \n".encode())
+                                    #Cant change
+                                    else:
+                                        rSock.send("/error Username already in use \n".encode())
+                            #Username is blank 
+                            elif uName.isspace() or uName == "":
+                                rSock.send("/error Please set a username first \n".encode())          
+                            else:
+                                if(command == "/say"):
+                                    #Write the message to logfile + send to all users
+                                    logFile.write("[" + getTime() + "] " + uName + ": " + data[len(command)+1:])
+                                    sendMessage(conns[rSock], message, rSock, s)
+                                elif(command == "/msg"):
+                                    #Find the recipients name from the data
+                                    recipientName = split[1]
+                                    message = message[len(recipientName)+1:]
+                                    #If valid user
+                                    if recipientName in conns.values():
+                                        #Message to specified user + write to log file
+                                        whisper(recipientName, message, uName)
+                                        logFile.write("[" + getTime() + "] <" + uName + " ->" + recipientName + ">: "+message)
+                                    else:
+                                        rSock.send("/error No user connected with that name\n".encode())
+                                else:
+                                    rSock.send("/error Command not valid \n".encode())               
+                    #Error with data
+                    except Exception as e:
+                        #Print on the error & left message
+                        print(str(e))
                         uName = conns[rSock]
                         print(uName + " has left")
-                        leaveMessage(uName,s)
-                        rSock.close()
+                        logFile.write("["+getTime()+"] " + uName + " has left"+"\n")
+                        #Delete the socket from dict and close it
                         del conns[rSock]
-
+                        rSock.close()
+                        #Inform other users
+                        leaveMessage(uName, s)
+                        
 def joinMessage(uName,sock,server):
-    message = "["+uName+"]"+" has joined!"
+    #Specify the message and print to console
+    message = "/info ["+uName+"]"+" has joined!"+"\n"
+    print(message)
+    #For every other user, send the message
     for client in conns.keys():
         if((client != server)&(client!=sock)):
             client.send(message.encode())
 
-def leaveMessage(uName,server):
-    message = "["+uName+"]"+" has left!"
+def leaveMessage(uName, server):
+    #Specify message and send to all clients
+    message = "/info ["+uName+"]"+" has left!"+"\n"
     for client in conns.keys():
         if((client != server)):
             client.send(message.encode())
             
-def sendMessage(uName,message,sock,server):
-    message = uName +": " + message
+            
+def sendMessage(uName, message, sock, server):
+    #Add the senders username and sent to all other clients
+    message = "/say <" + uName + "> " + ": " + message
+    for client in conns.keys():
+        if((client != server) & (client != sock)):
+            client.send(message.encode())
+
+
+def whisper(recipient, message, sender):
+    message = "/msg <"+sender+" -> You>"+": "+message
+    # In the list of usernames (conn values), find the index of the recipient's username, then the socket with be the corresponding index in keys
+    recipientSocket = list(conns.keys())[list(conns.values()).index(recipient)]
+    recipientSocket.send(message.encode())
+
+    
+def getTime():
+    #Use time module to get system time for logging 
+    t = time.localtime()
+    current_time = time.strftime("%H:%M:%S", t)
+    return current_time  
+    
+
+def sendUsers(uName, sock):
+    #Sends list of all usernames not in invalid
+    invalid = [uName, "SERVER"]
+    otherUsers = ""
+    for userName in conns.values():
+        if(userName not in invalid):
+            #Space seperated list
+            otherUsers += " "+userName
+    message = "/list"+str(otherUsers)
+    sock.send(message.encode())
+
+
+def updateUsers(oldUser, newUser, sock, server):
+    #Send message about username change to all other clients
+    message = "/info " + oldUser + " is now " + newUser + "\n"
     for client in conns.keys():
         if((client != server)&(client!=sock)):
             client.send(message.encode())
-        
-
-
 
 
 if __name__ == '__main__':
